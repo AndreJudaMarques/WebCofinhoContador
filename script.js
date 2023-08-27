01732328bb1b55228ce9b5dd20ef63406a305288
André Judá <judatrainer@gmail.com>
2023-08-27 18:56:12 -0300

    sav

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // script.js
 
+// Obtém o botão e a caixa de texto pelo ID
 const botaoMoeda1 = document.getElementById('botao-moeda-1');
-
 const caixaDeTexto = document.getElementById('caixa-de-texto');
 
 // Valor da moeda de 1 real
 const valorMoeda1Real = 1.0;
 
-
 // Adiciona um ouvinte de clique ao botão
 botaoMoeda1.addEventListener('click', () => {
-      // Atualiza a caixa de texto com o valor da moeda de 1 real
-      caixaDeTexto.value = valorMoeda1Real.toFixed(2);
-  });
+      console.log('botao da moeda 1 real clicado')
+    // Atualiza a caixa de texto com o valor da moeda de 1 real
+    caixaDeTexto.value = valorMoeda1Real.toFixed(2);
+});
