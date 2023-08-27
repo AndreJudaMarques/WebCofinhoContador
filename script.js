e10958f520e726bf18de2a1139abc40704086ef8
André Judá <judatrainer@gmail.com>
2023-08-27 19:29:39 -0300

    sa

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 // script.js
 
-// Obtém o botão e a caixa de texto pelo ID
-const botaoMoeda1 = document.getElementById('botao-moeda-1');
-const caixaDeTexto = document.getElementById('caixa-de-texto');
-
-// Valor da moeda de 1 real
-const valorMoeda1Real = 1.0;
-
-// Adiciona um ouvinte de clique ao botão
-botaoMoeda1.addEventListener('click', () => {
-      console.log('botao da moeda 1 real clicado')
-    // Atualiza a caixa de texto com o valor da moeda de 1 real
-    caixaDeTexto.value = valorMoeda1Real.toFixed(2);
-});
+// Função para adicionar valor à caixa de texto
+function adicionarValor(valor) {
+      var caixaDeTexto = document.getElementById("caixa-de-texto");
+      var valorAtual = parseFloat(caixaDeTexto.value) || 0;
+      caixaDeTexto.value = (valorAtual + valor).toFixed(2);
+  }
+  
